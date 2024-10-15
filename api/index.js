656029f81def56df6e840c81758d17c8b3dd386c
hlukare <hdlukare@gmail.com>
2024-10-15 22:10:52 +0530

    Index file Update and setting routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js'
+
 
 dotenv.config();
 
@@ -19,3 +21,5 @@ app.listen(3000, () =>
 {
     console.log("Server is running on port 3000");
 });
+
+app.use('/api/user', userRoutes);
